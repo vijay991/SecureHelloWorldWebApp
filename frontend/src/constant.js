6813b11914af740e48cd731c8cf8e16e15c5b35b
vijay991 <makwanavijay8267@gmail.com>
2023-11-28 19:40:18 +0530

    added logout api

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,3 +1,4 @@
 export const SIGNUP_URL = 'http://localhost:9002/api/signup'
 export const LOGIN_URL = 'http://localhost:9002/api/login'
 export const VERIFY_TOKEN_URL = 'http://localhost:9002/api/verify'
+export const LOGOUT_URL = 'http://localhost:9002/api/logout'
